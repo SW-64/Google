c23f4d35c5b70c102f08eecb0dde75e3f6aa177b
SW-64 <ettjddns6@gmail.com>
2024-07-19 22:43:26 +0900

    Comment : 二쇱꽍泥섎━

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,6 +9,9 @@ export class AppController {
   }
   constructor(private readonly appService: AppService) {}
 
+  // 구글 인증 가드  AuthGuard('google')
+  // UseGuards를 사용하면 특정 엔드포인트에 대한 접근권한 제어
+  // 즉 구글 인증 가드로 인해 인증 정보를 검증하고 성공해야만 googldAuth 메소드를 실행할 수 있다.
   @Get()
   @UseGuards(AuthGuard('google'))
   async googleAuth(@Req() req) {}
--- src/google/strategy/google.strategy.ts
+++ src/google/strategy/google.strategy.ts
@@ -1,39 +1,45 @@
-// import { PassportStrategy } from '@nestjs/passport';
-// import { Strategy, VerifyCallback } from 'passport-google-oauth20';
-// import { config } from 'dotenv';
-// import { ConfigService } from '@nestjs/config';
-// import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { config } from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
 
-// config();
+config();
 
-// @Injectable()
-// export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-//   constructor(private configService: ConfigService) {
-//     super({
-//       clientID: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_SECRET,
-//       callbackURL: 'http://localhost:3000/google/redirect',
-//       scope: ['email', 'profile'],
-//     });
-//   }
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private configService: ConfigService) {
+    super({
+      clientID: configService.get('GOOGLE_CLIENT_ID'),
+      clientSecret: configService.get('GOOGLE_SECRET'),
+      callbackURL: 'http://localhost:3000/google/redirect',
+      // Google이 사용자를 인증한 후 제어권을 반환하는 앱의 특정 엔드포인트
+      scope: ['email', 'profile'],
+    });
+  }
 
-//   async validate(
-//     accessToken: string,
-//     refreshToken: string,
-//     profile: any,
-//     done: VerifyCallback,
-//   ): Promise<any> {
-//     const { name, emails, photos } = profile;
-//     const user = {
-//       email: emails[0].value,
-//       firstName: name.familyName,
-//       lastName: name.givenName,
-//       picture: photos[0].value,
-//       accessToken,
-//     };
-//     done(null, user);
-//   }
-// }
+  //OAuth2 인증 과정 중 사용자 정보를 검증하는 로직
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { name, emails, photos } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.familyName,
+      lastName: name.givenName,
+      picture: photos[0].value,
+      accessToken,
+      //refreshToken 이건 출력이 되지 않는다.
+      //보안상의 이유로 리프레시 토큰을 클라이언트에게 전달하지 않는 것이 일반적인 관행
+    };
+    done(null, user);
+  }
+}
+
+/*
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { config } from 'dotenv';
@@ -70,3 +76,4 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     done(null, user);
   }
 }
+*/
